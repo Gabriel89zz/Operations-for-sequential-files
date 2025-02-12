5a5fd6c7817acc1e49e184b3872051a07e3e70c0
Gabriel Torres <elcomparosh97@gmail.com>
2025-02-11 21:17:21 -0600

    Agregar archivos de proyecto.

---
+++ Operations for sequential files/Form1.cs
@@ -0,0 +1,10 @@
+namespace Operations_for_sequential_files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
